b0b8fc6a1fe36b098b76347bc300b37dd018ee7f
fauzanfebrian <fauzanfebrian1725@gmail.com>
2023-12-06 14:32:16 +0700

    setup router

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import sendRouter from './router/send'
 import { errorHandler } from './util/handler'
 import { logout, printQR, status } from './whatsapp/controller'
+import authRouter from './auth/router'
 
 const app = express()
 
@@ -12,6 +13,7 @@ app.get('/qr-code', printQR)
 app.delete('/logout', logout)
 
 app.use('/send', sendRouter)
+app.use('/auth', authRouter)
 
 app.use(errorHandler)
 
